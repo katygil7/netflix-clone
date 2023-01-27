dcac24f855e4923d3b0c6bff2d765bb19f35eec0
katygil7 <gil.katherine22@gmail.com>
2023-01-26 19:19:29 -0500

    nav..

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -6,6 +6,34 @@ function HomeScreen() {
   return (
     <div className='homeScreen'>
         <Nav />
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+        <h1>Test</h1>
+
     </div>
   )
 }
