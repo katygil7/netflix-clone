14a8475ceb982234ef3ee451d3dd613abc0f7774
Katherine Gil <gil.katherine22@gmail.com>
2023-02-10 16:43:12 -0500

    understanding react router..

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import HomeScreen from './HomeScreen';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";"react-router-dom";
+
 
 
 function App() {
