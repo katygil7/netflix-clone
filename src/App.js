61bf8f5e8cac27a3fad1cbfe492dccecb18a16c9
Katherine Gil <gil.katherine22@gmail.com>
2023-02-10 16:44:32 -0500

    commented out the router import to run app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import HomeScreen from './HomeScreen';
 import './App.css';
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";"react-router-dom";
+// import {
+//   BrowserRouter,
+//   Routes,
+//   Route,
+// } from "react-router-dom";"react-router-dom";
 
 
 
